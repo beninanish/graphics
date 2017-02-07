3838a879f44b197514722260c97921ec2a29799a
ITLAB <ITLAB@IT208>
2017-02-07 15:41:52 +0530

    third commit

--- src/com/example/simple_graphics/MainActivity.java
+++ src/com/example/simple_graphics/MainActivity.java
@@ -14,18 +14,22 @@ public class MainActivity extends Activity {
     DemoView demoview;  
      
     @Override  
-    public void onCreate(Bundle savedInstanceState) {  
+    public void onCreate(Bundle savedInstanceState) 
+    {  
         super.onCreate(savedInstanceState);  
         demoview = new DemoView(this);  
         setContentView(demoview);  
     }  
   
-    private class DemoView extends View{  
-        public DemoView(Context context){  
+    private class DemoView extends View
+    {  
+        public DemoView(Context context)
+        {  
             super(context);  
         }  
   
-        @Override protected void onDraw(Canvas canvas) {  
+        @Override protected void onDraw(Canvas canvas) 
+        {  
             super.onDraw(canvas);  
   
            
@@ -35,5 +39,32 @@ public class MainActivity extends Activity {
            
             paint.setColor(Color.WHITE);  
             canvas.drawPaint(paint);  
+
+            
+            paint.setAntiAlias(false);  
+            paint.setColor(Color.BLUE);  
+            canvas.drawCircle(20, 20, 15, paint);  
+  
+            
+            paint.setAntiAlias(true);  
+            paint.setColor(Color.GREEN);  
+            canvas.drawCircle(60, 20, 15, paint);  
+  
+            
+            paint.setAntiAlias(false);  
+            paint.setColor(Color.RED);  
+            canvas.drawRect(100, 5, 200, 30, paint);  
+                           
+            
+            canvas.rotate(-45);  
+                      
+            paint.setStyle(Paint.Style.FILL);  
+            canvas.drawText("Graphics Rotation", 40, 180, paint);  
+              
+             
+            canvas.restore();  
+        }  
+    }  
+   
               
             
